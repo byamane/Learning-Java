611233ea5dd1a0b7dc467601bfd141f938e22362
byamane <byamane21@gmail.com>
2022-06-02 16:35:42 -0500

    complete section

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+package src;
+
 public class Main {
 
     public static void main(String[] args) {
-        double studentGPA = 3.45;
-        String studentFirstName = "Kayla";
-        String studentLastName = "Hammon";
+        double studentGPA = 3.53;
+        String studentFirstName = "Brandon";
+        String studentLastName = "Yamane";
+
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of: " + studentGPA);
     }
 }
