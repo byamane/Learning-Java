77b340112e637a47ebb0d1376dffbb718d1173a6
byamane <byamane21@gmail.com>
2022-06-02 19:25:52 -0500

    complete section

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,33 @@
+package src;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
-
+        String question = "What is the color of Clifford the big dog?";
+        String choiceOne = "Blue";
+        String choiceTwo = "Red";
+        String choiceThree = "Green";
+        
         String correctAnswer = choiceTwo;
-
+        
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
-
+        System.out.println(choiceOne + ", " + choiceTwo + ", " + choiceThree);
         // Have the user input an answer
+        Scanner input = new Scanner(System.in);
         // Retrieve the user's input
-
+        String userInput = input.next();
         // If the user's input matches the correctAnswer...
         // then the user is correct and we want to print out a congrats message to the user.
-
         // If the user's input does not match the correctAnswer...
         // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
-
+        if (correctAnswer.equals(userInput)){
+            System.out.println("Congrats! That is the correct answer");
+        } else {
+            System.out.println("Incorrect! The answer was Red!");
+        }
     }
-
 }
