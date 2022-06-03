83c795d021cee3c40307329bca4bfde35646c485
byamane <byamane21@gmail.com>
2022-06-03 10:24:58 -0500

    complete section

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+package src;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        double result = Math.pow(2, 5);
+        System.out.println(result);
+
+        Math.pow(2, 2);
     }
 
 }
